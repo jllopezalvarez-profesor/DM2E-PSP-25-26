c389fea61d1fafcc6d94a16e089869148572be7b
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2025-11-28 21:01:52 +0100

    ejemplos sockets

--- UT04-Comunicaciones/ejemplo-01-primeraconexion/src/main/java/es/jllopezalvarez/psp/ejemplossockets/ejemplo01primeraconexion/Client.java
+++ UT04-Comunicaciones/ejemplo-01-primeraconexion/src/main/java/es/jllopezalvarez/psp/ejemplossockets/ejemplo01primeraconexion/Client.java
@@ -6,7 +6,14 @@ import java.net.UnknownHostException;
 
 public class Client {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("localhost", 8000)){
+        try (
+
+                // Socket socket = new Socket()
+                Socket socket = new Socket("localhost", 8000)
+
+        ) {
+
+
             System.out.println("Conectado con el servidor");
 
         } catch (UnknownHostException e) {
