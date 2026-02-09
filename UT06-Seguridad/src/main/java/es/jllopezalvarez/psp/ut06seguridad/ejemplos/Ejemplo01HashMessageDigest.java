fba77b7fd92c7c5b28240c4f4dea613a6312f778
José Luis López Álvarez <jllopezalvarez@educa.madrid.org>
2026-02-10 00:42:26 +0100

    Ejemplos UT 06

---
+++ UT06-Seguridad/src/main/java/es/jllopezalvarez/psp/ut06seguridad/ejemplos/Ejemplo01HashMessageDigest.java
@@ -0,0 +1,62 @@
+package es.jllopezalvarez.psp.ut06seguridad.ejemplos;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Locale;
+import java.util.Random;
+
+import net.datafaker.Faker;
+
+public class Ejemplo01HashMessageDigest {
+    // Más nombres de algoritmos para MessageDigest en
+    // https://docs.oracle.com/en/java/javase/19/docs/specs/security/standard-names.html#messagedigest-algorithms
+    // private static final String HASH_ALGORITHM_NAME = "MD5";
+    // private static final String HASH_ALGORITHM_NAME = "SHA-1";
+    // private static final String HASH_ALGORITHM_NAME = "SHA-256";
+    // private static final String HASH_ALGORITHM_NAME = "SHA-512/256";
+    private static final String HASH_ALGORITHM_NAME = "SHA3-512";
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+
+        // Objeto Faker para generar textos aleatorios.
+        // Si lo inicializamos con una semilla concreta, los resultados se repiten
+        Faker faker = new Faker(Locale.forLanguageTag("es"), new Random(0));
+        // Se puede hacer con otros juegos de caracteres
+        // Faker faker = new Faker(Locale.forLanguageTag("ru"));
+
+        // Calculamos varios digests
+        for (int i = 0; i < 5; i++) {
+            // Creamos un texto para calcular su hash
+            // String texto = faker.starWars().quotes();
+            String texto = faker.name().fullName();
+
+            // Calculamos el digest
+            byte[] digest = calculaDigest(texto);
+
+            // Convertimos el digest a base64 para poder mostrarlo en pantalla
+            String digestB64 = Base64.getEncoder().encodeToString(digest);
+
+            // Mostramos los resultados:
+            System.out.printf("Algoritmo: %s\n", HASH_ALGORITHM_NAME);
+            System.out.printf("Mensaje: %s\n", texto);
+            System.out.printf("Longitud del digest binario: %s bytes\n", digest.length);
+            System.out.printf("Digest en Base64: %s\n", digestB64);
+            System.out.println("-".repeat(100));
+
+        }
+    }
+
+    private static byte[] calculaDigest(String mensaje) throws NoSuchAlgorithmException {
+        // Obtenemos una instancia de MessageDigest que usa el algoritmo deseado.
+        MessageDigest digest = MessageDigest.getInstance(HASH_ALGORITHM_NAME);
+
+        // Convertimos el mensaje en texto en un array de bytes.
+        byte[] bytesMensaje = mensaje.getBytes(StandardCharsets.UTF_8);
+
+        // Calculamos el digest y devolvemos el resultado
+        return digest.digest(bytesMensaje);
+    }
+
+}
